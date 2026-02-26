7f93854919ca00caa83616069ca7fdfd470a79e7
Quacent <quacent10@gmail.com>
2026-02-26 23:33:43 +0000

    Set up build environmet and basic executable

---
+++ src/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Preliminary Aircraft Stability Tool\n");
+    printf("-----------------------------------\n");
+    printf("Version 0.1\n");
+
+    return 0;
+}
